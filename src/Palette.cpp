2e65113d3638eb5c351f6bc863297d2e65b59968
vafdaf12 <vafdaf11@gmail.com>
2023-06-17 17:41:11 +0200

    feat: Only accept distinct colors in palette

--- src/Palette.cpp
+++ src/Palette.cpp
@@ -1,5 +1,9 @@
 #include "Palette.h"
 
+#include <algorithm>
+#include <cstdint>
+#include <iterator>
+#include <set>
 #include <fstream>
 #include <iomanip>
 #include <string>
@@ -13,12 +17,23 @@ std::ostream& operator<<(std::ostream& out, const Palette& p) {
     return out;
 }
 
+uint32_t compressRGB(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) {
+    return (r << 16) + (g << 8) + b;
+}
+glm::vec3 getVector(uint32_t rgb) {
+    uint8_t r = (rgb >> 16) & 255;
+    uint8_t g = (rgb >> 8) & 255;
+    uint8_t b = rgb & 255;
+
+    return glm::vec3(r, g, b) / 255.0f;
+}
+
 bool Palette::loadFromFile(const std::string& filename) {
     std::ifstream file(filename);
 
     if(!file) return false;
 
-    m_colors.clear();
+    std::set<uint32_t> set;
     std::string line;
     while(!file.eof()) {
         std::getline(file, line);
@@ -29,8 +44,14 @@ bool Palette::loadFromFile(const std::string& filename) {
 
         int r, g, b;
         sscanf(line.data(), "%02x%02x%02x", &r, &g, &b);
-        m_colors.push_back(glm::vec3(r, g, b) / 255.0f);
+        set.insert(compressRGB(r, g, b));
     }
     file.close();
+
+    // copy + map distinct elements to array
+    m_colors.clear();
+    m_colors.reserve(set.size());
+    std::transform(set.begin(), set.end(), std::back_inserter(m_colors), getVector);
+
     return true;
 }
