b3ef92ff51ccffca7963c2c2d31f6ab2e323e628
vafdaf12 <vafdaf11@gmail.com>
2023-07-05 20:10:35 +0200

    feat: Instance-level debug messages

--- src/vulkan/Vulkan.cpp
+++ src/vulkan/Vulkan.cpp
@@ -27,6 +27,34 @@ const std::vector<const char*> EXTENSIONS = {
 #endif
 };
 
+VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData
+) {
+    if(messageSeverity < VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) return VK_FALSE;
+    std::cout << "[VALIDATION] ";
+    std::cout << pCallbackData->pMessage << std::endl;
+    return VK_FALSE;
+}
+VkDebugUtilsMessengerCreateInfoEXT debugMessengerInfo() {
+    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity =
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+    createInfo.messageType =
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+    createInfo.pfnUserCallback = debugCallback;
+    return createInfo;
+}
+
 bool Vulkan::supportsLayers(const std::vector<const char*>& layers) const {
     std::vector<vk::LayerProperties> availableLayers;
     availableLayers = vk::enumerateInstanceLayerProperties();
@@ -88,12 +116,15 @@ void Vulkan::createInstance() {
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
 
+    VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = debugMessengerInfo();
+
     if(ENABLE_VALIDATION) {
         createInfo.enabledLayerCount = VALIDATION_LAYERS.size();
         createInfo.ppEnabledLayerNames = VALIDATION_LAYERS.data();
 
         createInfo.enabledExtensionCount = EXTENSIONS.size();
         createInfo.ppEnabledExtensionNames = EXTENSIONS.data();
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &messengerCreateInfo;
     }
 
     VkInstance instance;
@@ -104,30 +135,8 @@ void Vulkan::createInstance() {
     m_instance = vk::Instance(instance);
 }
 
-VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-    VkDebugUtilsMessageTypeFlagsEXT messageType,
-    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-    void* pUserData
-) {
-    std::cout << "--- VALIDATION LAYER ---\n";
-    std::cout << pCallbackData->pMessage << std::endl;
-    return VK_FALSE;
-}
 void Vulkan::setupDebugMessenger() {
-    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    createInfo.messageSeverity =
-        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
-        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-
-    createInfo.messageType =
-        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
-        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-
-    createInfo.pfnUserCallback = debugCallback;
+    VkDebugUtilsMessengerCreateInfoEXT createInfo = debugMessengerInfo();
 
     VkDebugUtilsMessengerEXT messenger;
     vk::ext::createDebugUtilsMessengerEXT(m_instance, &createInfo, nullptr, &messenger);
